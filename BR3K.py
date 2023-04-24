d1de72c46b85ba6c0503957967467aad9ee81b59
snad Alswd <125707436+miggyhg@users.noreply.github.com>
2023-04-24 20:55:44 +0200

    cd snad
    snad

---
+++ BR3K.py
@@ -0,0 +1,87 @@
+# الاداه مجانيه متحاولش تبزنس فيها
+# تمت البرمجه من قبل زياد العباني كل شي في تيليغرام
+
+
+
+
+
+
+
+
+import os
+print("""\033[0;33m 
+    .--'''''''''--.
+     .'      .---.      '.
+    /    .-----------.    \
+   /        .-----.        \
+   |       .-.   .-.       |
+   |      /   \ /   \      |
+    \    | .-. | .-. |    /
+     '-._| | | | | | |_.-'
+         | '-' | '-' |
+          \___/ \___/
+       _.-'  /   \  `-._
+     .' _.--|     |--._ '.
+     ' _...-|     |-..._ '
+            |     |
+            '.___.'
+              | |
+             _| |_
+            /\( )/\
+           /  ` '  \
+          | |     | |
+          '-'     '-'
+          | |     | |
+          | |     | |
+          | |-----| |
+       .`/  |     | |/`.
+       |    |     |    |
+       '._.'| .-. |'._.'
+             \ | /
+             | | |
+             | | |
+             | | |
+            /| | |\
+          .'_| | |_`.
+BR3K      `. | | | .'
+       .    /  |  \    .
+      /o`.-'  / \  `-.`o\
+     /o  o\ .'   `. /o  o\
+     `.___.'       `.___.'
+     """)
+
+print(" [1] pkg Upgrade")
+print(" [2] pkg install git")
+print(" [3]pip install rich")
+print(" [4]pip install requests")
+print(" [5]pip install bs4")
+print(" [6]MY FACEBOOK[ZEYAD ALABANY]")
+print(" [7]MY TELE [SXTZ]")
+print(" [8]EXIT")
+sxtz =input("choice :")
+if sxtz in ["1", "01"]:
+    os.system('pkg upgrade')
+    os.system('python BR3K.py')
+if sxtz in ["2", "02"]:
+    os.system('pkg install git')
+    os.system('python BR3K.py')
+if sxtz in ["3", "03"]:
+    os.system('pip install rich')
+    os.system('python BR3K.py')
+if sxtz in ["4", "04"]:
+    os.system('pip install requests')
+    os.system('python BR3K.py')
+if sxtz in ["5", "05"]:
+    os.system('pip install bs4')
+    os.system('python BR3K.py')
+if sxtz in ["6", "06"]:
+    os.system('xdg-open https://www.facebook.com/Zebany.ReD.X/')
+    os.system('python BR3K.py')
+if sxtz in ["7", "07"]:
+    os.system('xdg-open https://t.me/sxtz0')
+    os.system('python BR3K.py')
+if sxtz in ["8", "08"]:
+    exit()   
+  
+    
+    
